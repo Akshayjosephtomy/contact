ecf23da1d35a5d1afce3e8291eee6f6eb52c5715
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-15 11:35:34 +0530

    contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewcontactComponent } from './viewcontact/viewcontact.component';
+
+const link:Routes=[
+  {
+    path:"",component:ViewcontactComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -12,7 +19,8 @@ import { ViewcontactComponent } from './viewcontact/viewcontact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(link)
   ],
   providers: [],
   bootstrap: [AppComponent]
